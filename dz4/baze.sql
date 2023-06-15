6e27ea32427d92528737c826f85a8af0f9de8b52
Uidserg <uidserg@live.com>
2023-06-15 23:31:56 +0500

    дз4

---
+++ dz4/baze.sql
@@ -0,0 +1,19 @@
+-- create
+CREATE TABLE STUDENTS (
+  empId INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  adress TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO STUDENTS VALUES (0001, 'Сергей', 34, 'Пермь');
+INSERT INTO STUDENTS VALUES (0002, 'Иван', 18, 'Астрахань');
+INSERT INTO STUDENTS VALUES (0003, 'Ирина', 19, 'Брянск');
+INSERT INTO STUDENTS VALUES (0004, 'Андрей', 20, 'Владимир');
+INSERT INTO STUDENTS VALUES (0005, 'Борис', 21, 'Братск');
+INSERT INTO STUDENTS VALUES (0006, 'Вероника', 22, 'Курск');
+INSERT INTO STUDENTS VALUES (0007, 'Галина', 23, 'Москва');
+INSERT INTO STUDENTS VALUES (0008, 'Диана', 24, 'Питер');
+INSERT INTO STUDENTS VALUES (0009, 'Константин', 25, 'Владивосток');
+INSERT INTO STUDENTS VALUES (0010, 'Дмитрий', 26, 'Иркутск');
